eacd8c5ccaf87b8e4fc3025e32e5b5c273642515
Julian Lihué Sanchez de Freitas <julihue.sanchez03@gmail.com>
2025-04-03 14:39:55 -0300

    Modifico la funcionalidad para imprimir mi tabla

--- src/imprimirRonda/__init__.py
+++ src/imprimirRonda/__init__.py
@@ -1,4 +1,14 @@
+def estructura_ordenada(diccionario):
+    valores_ordenados = []
+    for keys, value in diccionario.items():
+        valores_ordenados.append((keys, value))
+    valores_ordenados = sorted(valores_ordenados, key= lambda c : c[1]["puntajes"], reverse=True)
+    print(valores_ordenados)
+        
+
+    return valores_ordenados
 def imprimir_ronda(diccionario):
+    estructura_nueva = estructura_ordenada(diccionario)
     """ Esta funcion imprime una ronda de un diccionario"""
     print(f"Jugador    | Asesinatos       | Asistencias     | Muertes     | MVPs       | Puntaje   ")
     for nombre in diccionario.items():
