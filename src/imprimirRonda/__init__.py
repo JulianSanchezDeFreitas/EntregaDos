721e8b2dd52438d44c3afa984ab3d6b7c06d1137
Julian Lihué Sanchez de Freitas <julihue.sanchez03@gmail.com>
2025-04-03 14:54:23 -0300

    Actualizacion en la forma de imprimir mi estructura

--- src/imprimirRonda/__init__.py
+++ src/imprimirRonda/__init__.py
@@ -1,15 +1,14 @@
 def estructura_ordenada(diccionario):
+    """ Esta funcion recibe un diccionario por parametro y devuelve en su lugar una lista de tuplas ordenadas por puntaje"""
     valores_ordenados = []
     for keys, value in diccionario.items():
         valores_ordenados.append((keys, value))
     valores_ordenados = sorted(valores_ordenados, key= lambda c : c[1]["puntajes"], reverse=True)
-    print(valores_ordenados)
-        
-
     return valores_ordenados
+
 def imprimir_ronda(diccionario):
-    estructura_nueva = estructura_ordenada(diccionario)
     """ Esta funcion imprime una ronda de un diccionario"""
+    tabla_ordenada = estructura_ordenada(diccionario)
     print(f"Jugador    | Asesinatos       | Asistencias     | Muertes     | MVPs       | Puntaje   ")
-    for nombre in diccionario.items():
-        print(f"{nombre[0]}   |         {nombre[1]["kills"]}     |          {nombre[1]["assists"]}      |       {nombre[1]["deaths"]}        |      {nombre[1]["MVPs"]}       |         {nombre[1]["puntajes"]}")
+    for jugador in tabla_ordenada:
+        print(f"{jugador[0] if len(jugador[0])==6 else (f"{jugador[0]} ")}   |         {jugador[1]["kills"]}     |          {jugador[1]["assists"]}      |       {jugador[1]["deaths"]}        |      {jugador[1]["MVPs"]}       |      {jugador[1]["puntajes"]}")
